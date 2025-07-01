06ec999fb8a5bf1933dd5cae60d14d6d3373c94e
maundjs <maundjs@gmail.com>
2025-07-01 15:38:15 +0100

    Keymaps for error warnigns

--- .config/nvim/lua/maund/keymaps.lua
+++ .config/nvim/lua/maund/keymaps.lua
@@ -2,6 +2,13 @@ vim.g.mapleader = " "
 
 local function map(mode, lhs, rhs)
     vim.keymap.set(mode, lhs, rhs, { silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true }
+    vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+    -- The following command requires plug-ins "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", and optionally "kyazdani42/nvim-web-devicons" for icon support
+    vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+    -- If you don't want to use the telescope plug-in but still want to see all the errors/warnings, comment out the telescope line and uncomment this:
+    -- vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
 end
 
 
